20839659c752f4a99e065fe566ae452146d3c4a5
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2024-11-17 14:10:59 +0100

    Delete codes directory

--- codes/lab0/HelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int c, char **a)
-{
-printf("Hello_World\n");
-return 0;
-}
-
--- codes/lab0/testgedit.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char **argv)
-{
-int counter=10;
-while(--counter>=0) printf("Counter=%d\n",counter);
-exit(0);
-}
-
