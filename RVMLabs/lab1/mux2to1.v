48030fe05d367651f3a80ee11ab2761680cbeed8
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2024-11-30 16:39:38 +0100

    Delete RVMLabs/lab1 directory

--- RVMLabs/lab1/mux2to1.v
+++
@@ -1,9 +0,0 @@
-module mux2to1 (
-    input wire a,      // Input A
-    input wire b,      // Input B
-    input wire sel,    // Select line
-    output wire y      // Output Y
-);
-assign y = (sel) ? b : a;   // If sel = 1, choose B; otherwise, choose A
-endmodule
-
--- RVMLabs/lab1/simple_module.v
+++
@@ -1,5 +0,0 @@
-// simple_module.v
-module simple_module(input a, input b, output y);
-    assign y = a & b;  // AND gate
-endmodule
-
